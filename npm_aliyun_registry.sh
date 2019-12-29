6547f75cea44473058a8da6bf2cf75649221040e
Velne Wang <velnewang@gmail.com>
2019-12-30 07:54:33 +0800

    ADD: add some properties.

---
+++ npm_aliyun_registry.sh
@@ -0,0 +1,6 @@
+npm install phantomjs --phantomjs_cdnurl=http://npm.taobao.org/mirrors/phantomjs
+npm install chromedriver --chromedriver_cdnurl=http://npm.taobao.org/mirrors/chromedriver
+npm install operadriver --operadriver_cdnurl=http://npm.taobao.org/mirrors/operadriver
+
+# npm config set registry http://registry.npmjs.org
+npm config set registry https://registry.npm.taobao.org
---
+++ npm_azure_registry.sh
@@ -0,0 +1,5 @@
+npm install phantomjs --phantomjs_cdnurl=http://npm.taobao.org/mirrors/phantomjs
+npm install chromedriver --chromedriver_cdnurl=http://npm.taobao.org/mirrors/chromedriver
+npm install operadriver --operadriver_cdnurl=http://npm.taobao.org/mirrors/operadriver
+
+npm config set registry http://crproxy.trafficmanager.net:4873
